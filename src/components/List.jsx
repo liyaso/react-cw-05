023cd4495ddb6c82324fd5b7b59db421810be77b
liyaso <118847571+liyaso@users.noreply.github.com>
2026-04-21 13:26:35 -0400

    Styled List

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 
 class List extends Component{
     render(){
+        if(this.props.items.length === 0){
+            return <p className='produce-list-empty'>No items match your search.</p>
+        }
         return(
-            <ul>
+            <ul className='produce-list'>
                 {this.props.items.map(item => (
                     <li key={item.name}>{item.name}</li>
                 ))}
